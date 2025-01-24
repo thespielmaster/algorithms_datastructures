cda1864a4e64b0989faaa52f0c811e94bd42774e
Victor Sanchez <thespielmaster@MacBook-Pro-de-Victor.local>
2025-01-24 13:47:12 +0100

    working singly linked list

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include "sorting.hpp"
+#include "linked_list.hpp"
 
 int main(){
 
     std::vector<int> arr = {3, 2, 4, 1, 6};
 
+    LinkedList<int> myList;
+    myList.addAtHead(1);
+
     for(auto it = arr.begin(); it != arr.end(); it++){
         std::cout << *it << std::endl;
+        myList.addAtTail(*it);
     }
 
-
-    Sorting<int> sort(arr);
-    std::vector<int> sorted = sort.mergeSort();
-
-    for(auto it = sorted.begin(); it != sorted.end(); it++){
-        std::cout << *it << std::endl;
-    }
+    myList.print();
     
     return 0;
 }
